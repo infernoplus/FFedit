1dc41cc03d813d71c1b910affb0c56dd099e3e36
Patrick <dmtinfernocide@gmail.com>
2015-11-20 18:07:23 -0600

    Update input.js

--- js/input.js
+++ js/input.js
@@ -37,18 +37,83 @@ input.onMouse = function(evt) {
 };
 
 input.getWidthField = function() {
-	return document.getElementById("width").value;
+	return parseInt(document.getElementById("width").value);
 };
 
 input.getHeightField = function() {
-	return document.getElementById("height").value;
+	return parseInt(document.getElementById("height").value);
 };
 
 input.editCollision = function() {
+	editor.editEvent = false; document.getElementById("event").checked = false;
+	editor.editObject = false; document.getElementById("object").checked = false;
 	editor.editCollision = document.getElementById("collision").checked;
 
+  input.toolbarToggle();
 };
 
 input.editEvent = function() {
+	editor.editCollision = false; document.getElementById("collision").checked = false;
+	editor.editObject = false; document.getElementById("object").checked = false;
 	editor.editEvent = document.getElementById("event").checked;
+
+	input.toolbarToggle();
+	editor.redrawEventSelection();
+};
+
+input.editObject = function() {
+	editor.editEvent = false; document.getElementById("event").checked = false;
+	editor.editCollision = false; document.getElementById("collision").checked = false;
+	editor.editObject = document.getElementById("object").checked;
+
+	input.toolbarToggle();
+	editor.redrawObjectSelection();
+};
+
+input.editEventMode = function() {
+	var m = document.getElementById("mapEditor");
+	var e = document.getElementById("eventEditor");
+	var o = document.getElementById("objectEditor");
+	m.style.display = 'none';
+	e.style.display = 'block';
+	o.style.display = 'none';
+
+	editor.redrawEventList();
+	editor.redrawObjectList();
+};
+
+input.editObjectMode = function() {
+	var m = document.getElementById("mapEditor");
+	var e = document.getElementById("eventEditor");
+	var o = document.getElementById("objectEditor");
+	m.style.display = 'none';
+	e.style.display = 'none';
+	o.style.display = 'block';
+
+	editor.redrawEventList();
+	editor.redrawObjectList();
+};
+
+input.editMapMode = function() {
+	var m = document.getElementById("mapEditor");
+	var e = document.getElementById("eventEditor");
+	var o = document.getElementById("objectEditor");
+	m.style.display = 'block';
+	e.style.display = 'none';
+	o.style.display = 'none';
+
+	editor.saveEventList();
+	editor.saveObjectList();
+	editor.redrawEventSelection();
+	editor.redrawObjectSelection();
+};
+
+input.toolbarToggle = function() {
+	var ts = document.getElementById("tileList");
+	var es = document.getElementById("eventSelection");
+	var os = document.getElementById("objectSelection");
+
+	ts.style.display = !editor.editCollision && !editor.editEvent && !editor.editObject ? 'block' : 'none';
+	es.style.display = !editor.editCollision && editor.editEvent && !editor.editObject ? 'block' : 'none';
+	os.style.display = !editor.editCollision && !editor.editEvent && editor.editObject ? 'block' : 'none';
 };
